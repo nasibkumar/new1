2e9e7e3a36d12ec23549af7dc4f1ee5194b4d26a
nasibkumar <60296131+nasibkumar@users.noreply.github.com>
2020-05-30 22:52:36 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,49 @@
+<html>
+<title>Hello, world!</title>
+  </head>
+  <body style="background-image:url(img2.jpg);">
+  <br>
+  <center>
+  <div style="background-color:rgba(255,255,255,0.3); margin-left:400px; margin-right:400px; margin-top:40px; padding:30px;
+  border-top-left-radius:50px; border-bottom-right-radius:50px; box-shadow:10px 10px 2px 5px orange; font-size:25px;">
+<b><form action="addsave.php" method="POST">
+    <label style="margin-right:110px;">Name:</label>
+    <br>
+    <input type="username" name="name2" required>
+    <br>
+    <br>
+  <label style="margin-right:140px;">ID:</label>
+  <br>
+    <input type="text" name="id" required>
+    <br>
+    <br>
+   <label style="margin-right:80px;">Address:</label>
+   <br>
+   <textarea style="margin-left:130px;" rows="8" cols="40" name="add" style="overflow:scroll;">         		
+         </textarea>
+    <br>
+    <br>
+   <label style="margin-right:60px;">Phone No:</label>
+   <br>
+    <input  name="pho" required>
+    <br>
+    <br>
+   <label style="margin-right:40px;">Department:</label>
+   <br>
+    <input type="text" name="dept" required>
+   <br>
+   <br>
+   <button style="margin-right:40px; background-color:blue; font-size:20px; color:black;" type="submit" name="sub10">Add Yourself</button>
+</form>
+</b>
+</div>
+</center>
+  </body>
+</html>
+
+
+
+
+
+
+
